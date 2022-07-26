1a3b92a9e813dd836a6e7e2a98a8f6d5248a7dbc
Staxxx <sersersin@gmail.com>
2022-07-27 00:59:31 +0300

    add code

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿//Пользователь вводит число a
 Console.Write("Введите a = ");
 int a = Convert.ToInt32(Console.ReadLine());
+
+if ( a % 2 < 0)
+{
+  Console.Write("да");  
+}
+else
+{
+  Console.Write("нет");  
+}
