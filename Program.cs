40abbfc076e473893ac0bb29ca06422f06413ac6
Staxxx <sersersin@gmail.com>
2022-07-27 00:57:36 +0300

    add drawio

--- Program.cs
+++ Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Пользователь вводит число a
+Console.Write("Введите a = ");
+int a = Convert.ToInt32(Console.ReadLine());
